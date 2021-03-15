f9f0216fac961be4f4cb1ebc26732dcbb1720ae3
ayazhan8 <54767127+ayazhan8@users.noreply.github.com>
2021-03-16 01:02:17 +0600

    No negative numbers handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var reversed = 0;
+
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    while (n > 0) {
+        reversed = (n % 10) + reversed * 10;
+        n = parseInt(n / 10);
+    }
+
+    return reversed;
+};
